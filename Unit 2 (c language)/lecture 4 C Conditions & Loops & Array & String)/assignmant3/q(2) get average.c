fc328e1414f74b3f260925ba233512d2731611a1
aymanGama <aymangmal12345678@gmail.com>
2022-07-22 23:28:28 +0300

    questiont2 average

---
+++ Unit 2 (c language)/lecture 4 C Conditions & Loops & Array & String)/assignmant3/q(2) get average.c
@@ -0,0 +1,29 @@
+/* get average for n numbers  entered by user
+ * main.c
+ *
+ *  Created on: ??ş/??ş/????
+ *      Author: ayman
+ */
+#include<stdio.h>
+int main(){
+	int i,n; float average ,sum ;   //define variable
+	float avr[100];
+	printf("Enter the number of elements : ");
+	fflush (stdout);  fflush (stdin);   //ask user to give number
+	scanf("%d",&n);
+	if (n<=0 || n>100 ){
+		printf("error you should inter number from 0 t0 100");
+	}
+	else if (0<n&&n<=100){
+	for(i=0;i<n;i++){
+		printf("Enter number N%d : ",i+1);
+		fflush (stdout);  fflush (stdin);
+		scanf("%f",&avr[i]);
+	}
+	for(i=0,sum=0;i<n;i++){            //sum  array numbers
+		sum+=avr[i];
+		}
+	average = sum/n;                  //get average and print it
+	printf("\n the average for %d numbers =  %.2f",n,average);
+	}
+return 0; }
